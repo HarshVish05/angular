02cbeda115933d1e8f4592dae1fcce2f5d46eed5
harsh <codeharshit05@gmail.com>
2023-04-06 08:42:59 +0530

    first angular try

---
+++ src/app/components/button/button.component.ts
@@ -0,0 +1,27 @@
+import { Component, OnInit,Output,EventEmitter } from '@angular/core';
+
+@Component({
+  selector: 'app-button',
+  templateUrl: './button.component.html',
+  styleUrls: ['./button.component.css']
+})
+export class ButtonComponent implements OnInit {
+
+
+  @Output() btnClick = new EventEmitter();
+
+  constructor(){
+
+  }
+
+  ngOnInit(): void{
+
+  }
+
+  OnClick(){
+    console.log('onclick workd');
+    
+    this.btnClick.emit()
+  }
+
+}
